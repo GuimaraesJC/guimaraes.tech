91dfea3e0814fede857f983fd01e4dd8243f1f80
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-03-15 20:12:49 -0300

    Configures styled-components and adds global style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,3 @@
-import Head from 'next/head';
-
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Jean Carlos Guimarães</title>
-        <meta name="description" content="Meu blog pessoal." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>Hello</h1>
-      </main>
-    </>
-  );
+  return <h1>Hello World</h1>;
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+    font-size: 16px;
+  }
+
+  *,
+  *:after,
+  *:before {
+    box-sizing: inherit;
+  }
+
+  body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  a,
+  button {
+    cursor: pointer;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+  }
+`;
+
+export default GlobalStyle;
