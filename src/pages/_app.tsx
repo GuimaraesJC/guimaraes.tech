1681242a9bd544615178f454b41d9267afdc867e
Jean Carlos Costa <guimaraes.jean95@gmail.com>
2023-03-15 20:31:51 -0300

    Fixing SEO with Head component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 import GlobalStyle from '@/styles/global';
@@ -5,6 +6,12 @@ import GlobalStyle from '@/styles/global';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Jean Carlos Guimarães</title>
+        <meta name="description" content="Meu blog pessoal." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
